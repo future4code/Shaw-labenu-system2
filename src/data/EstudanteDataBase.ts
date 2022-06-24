7ba44d2763e2d7883ee101945a80d1537d20bfb2
Renan Martinez <remam_martinez89@hotmail.com>
2022-06-24 14:57:27 -0300

    criado endpoint trocarEstudante

--- src/data/EstudanteDataBase.ts
+++ src/data/EstudanteDataBase.ts
@@ -2,39 +2,43 @@ import { Estudante } from "../model/Estudante";
 import { BaseDatabase } from "./BaseDatabase";
 
 
-export class EstudanteDataBase extends BaseDatabase{
+export class EstudanteDataBase extends BaseDatabase {
   public async criarEstudante(estudante: Estudante) {
-    
     try {
-      await BaseDatabase.connection("Estudante")
-      .insert({
+      await BaseDatabase.connection("Estudante").insert({
         nome: estudante.getNome(),
         email: estudante.getEmail(),
         data_nasc: estudante.getData_Nasc(),
         turma_id: estudante.getTurma_id(),
-      })
+      });
 
-      await BaseDatabase.connection("Hobby")
-      .insert({
+      await BaseDatabase.connection("Hobby").insert({
         nome: estudante.getHobby(),
-      })
-    
-    } catch (error:any) {
-      console.log(error.sqlMessage)
-      throw new Error(error.sqlMessage || error.message)
+      });
+    } catch (error: any) {
+      console.log(error.sqlMessage);
+      throw new Error(error.sqlMessage || error.message);
     }
   }
 
   public async pegarEstudante() {
-    
     try {
-      const result = await BaseDatabase.connection("Estudante")
-      .select("*")
+      const result = await BaseDatabase.connection("Estudante").select("*");
 
-      await BaseDatabase.connection("Hobby")
-      .select("*")
-      return result
-    
+      await BaseDatabase.connection("Hobby").select("*");
+      return result;
+    } catch (error: any) {
+      throw new Error(error.sqlMessage || error.message);
+    }
+  }
+
+  public async mudarTurmaEstudante(id: number, turma_id: number) {
+    try {
+      await BaseDatabase.connection("Estudante")
+        .update({
+          turma_id: turma_id
+        })
+        .where("id", id);
     } catch (error:any) {
       throw new Error(error.sqlMessage || error.message)
     }
--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,14 @@ import { buscarDocente } from "./endpoints/BuscarDocentes";
 import { createDocente } from "./endpoints/CriarDocente";
 import { createTurma } from "./endpoints/CriarTurma";
 import { trocaDocente } from "./endpoints/TrocarDocente";
+import { trocaEstudante } from "./endpoints/TrocarEstudante";
 
 app.post("/criarturma", createTurma);
-
 app.post("/criarestudante", createEstudante);
-
-app.get("/pegarestudante", pegarEstudante);
-
 app.post("/criardocente", createDocente);
 
+app.get("/pegarestudante", pegarEstudante);
 app.get("/buscardocente", buscarDocente);
 
 app.put("/trocadocente", trocaDocente);
+app.put("/trocaestudante", trocaEstudante);
