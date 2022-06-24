d6f0560cc9ef364e7fbbaabf3b1df3a83458a545
Renan Martinez <remam_martinez89@hotmail.com>
2022-06-24 15:30:13 -0300

    criada função para mudar formato da data de BR para ING

--- src/endpoints/CriarEstudante.ts
+++ src/endpoints/CriarEstudante.ts
@@ -2,18 +2,24 @@ import { Request, Response } from "express";
 import { EstudanteDataBase } from "../data/EstudanteDataBase";
 import { Estudante } from "../model/Estudante";
 
-export const createEstudante = async(req: Request, res: Response): Promise<void> => {
+export const createEstudante = async (req: Request,res: Response): Promise<void> => {
   try {
-    const {nome, email, nascimento, turma_id, hobby} = req.body
+    const { nome, email, nascimento, turma_id, hobby } = req.body;
 
-    const estudante = new Estudante(nome, email, nascimento, turma_id, hobby)
-      
-    const turmaDB = new EstudanteDataBase()
+    const convertDate = (date: string): string => {
+      const arrDate = date.split("/");
+      return `${arrDate[2]}-${arrDate[1]}-${arrDate[0]}`;
+    };
+    const dateIng = convertDate(nascimento);
 
-    await turmaDB.criarEstudante(estudante)
+    const estudante = new Estudante(nome, email, dateIng, turma_id, hobby);
 
-     res.status(201).send("Criado estudante com sucesso")
-  } catch (error:any) {
-    res.status(500).send(error.message)
+    const turmaDB = new EstudanteDataBase();
+
+    await turmaDB.criarEstudante(estudante);
+
+    res.status(201).send("Criado estudante com sucesso");
+  } catch (error: any) {
+    res.status(500).send(error.message);
   }
-}
+};
