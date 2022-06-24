c703cef4529b6802228507e802157ee574a42193
marcos <marcos-dido@hotmail.com>
2022-06-24 18:05:44 -0300

    turma-feita

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { app } from "./app";
+import { getTurmas } from "./endpoints/BuscarTurmas";
 import { createTurma } from "./endpoints/CriarTurma";
+import { mudarModulo } from "./endpoints/MudarModulo";
 
 
 
+app.get('/buscarTurmasAtivas', getTurmas)
 app.post('/criarturma', createTurma)
+app.put('/mudarModulo/:id', mudarModulo)
 
 
 
